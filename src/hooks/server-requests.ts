05fe2ca8f37eb797f0a5e92a7c2b861ef268cae2
Inbal Kimhi <inbalkm100@gmail.com>
2022-12-28 19:17:47 +0200

    finished assigment

--- src/hooks/server-requests.ts
+++ src/hooks/server-requests.ts
@@ -9,17 +9,9 @@ const endpoint = '../assets/'; // todo: add endpoint (server) address (starting 
  **/
 export async function getMessages() {
   // todo: replace this with fetch to get the messages from the server
-  const { mockMessages } = await import(`${endpoint}/mockMessages`);
-
-  // todo: this should be implemented in the server. Chat Messages should already have the authors' names.
-  // todo: remove this mapping when getting the data from the server
-  const mockMessagesWithNames = mockMessages.map((message: Message) => {
-    const author = mockUsers.find(user => user.id === message.authorId);
-    const authorName = author && author.name;
-    return { ...message, authorName };
-  });
-
-  return mockMessagesWithNames;
+  const userNames = await fetch('http://localhost:3001/Name')
+  const res = await userNames.json()
+  return res;
 }
 
 /**
@@ -27,10 +19,14 @@ export async function getMessages() {
  **/
 export async function getUsers() {
   // todo: replace this with fetch to get the user list from the server
-  const { mockUsers } = await import(`${endpoint}/mockUsers`);
-  return mockUsers;
+  const  mockUsers= await fetch('http://localhost:3001/useridName');
+  const res =  await mockUsers.json();
+  return res ;
 }
 
+interface User{
+  id : number
+}
 
 /**
  * GET request to get the full details of a user
@@ -39,8 +35,11 @@ export async function getUserDetails(userId: number) {
   // todo: replace this with fetch to get the user details from the server.
   //  For mocking example, we're calling an external JSON service.
   //  You can use mockUserDetails.ts for the list of user details in the server.
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users?id=${userId}`);
-  return (await res.json())[0];
+  const usersDetails = await fetch('http://localhost:3001/userdetails');
+  const res: User[] = await usersDetails.json();
+  const ans = res.filter( user => user.id === userId)
+  return ans[0]
+  
 }
 
 /**
@@ -48,6 +47,13 @@ export async function getUserDetails(userId: number) {
  **/
 export async function addNewMessage(message: Message) {
   // todo: implement sending a new message to the server
+  const response = await fetch('http://localhost:3001/newmessage', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(message) 
+  });
 }
 
 /**
@@ -55,4 +61,11 @@ export async function addNewMessage(message: Message) {
  **/
 export async function changeMessageLikes(messageId: number, userId: number, like: boolean) {
   // todo: implement sending a rquest to change the like of a message by the user
-}
+  const response = await fetch('http://localhost:3001/like', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({messageId, userId, like})
+
+  })}
